81131842e24f24cb97e0fd3b315ab9eee96a7aea
Lakshay Gupta <lakshaygpt28@gmail.com>
2024-07-06 11:17:37 +0530

    Add spring boot boiler plate code

---
+++ src/test/java/com/lakshaygpt28/bookmyticket/BookMyTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshaygpt28.bookmyticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
